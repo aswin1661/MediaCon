03aa37148a22ac3661a76677e174bf12308c58e2
Aswin S <aswinsanthoshachus@gmail.com>
2025-03-23 19:38:21 +0530

    Update quote.tsx

--- src/components/quote.tsx
+++ src/components/quote.tsx
@@ -9,7 +9,7 @@ export const SpeakerQuote = ({ children, data, sub, position = "left", className
 }) => {
     return (
         <div class={cn("flex flex-col gap-10 md:gap-20 group items-center w-full justify-center", position === "left" ? "md:flex-row-reverse" : "md:flex-row", className)}>
-            <PhotoPic className={cn("w-64 md:w-96 group-hover:scale-105 transition-all duration-500", position === "right" ? "md:ms-20 -rotate-12 group-hover:-rotate-6" : "md:me-20 rotate-12 group-hover:rotate-6")} {...data} />
+            <PhotoPic className={cn("w-64 md:w-96 group-hover:scale-105 mt-[6vh] transition-all duration-500", position === "right" ? "md:ms-20 -rotate-12 group-hover:-rotate-6" : "md:me-20 rotate-12 group-hover:rotate-6")} {...data} />
             <div class={cn("w-full md:w-3/4 lg:w-3/5 px-3 md:px-0 flex flex-col gap-3", position === "left" ? "items-end text-end" : "items-start text-start")}>
                 <Text class="opacity-75 w-8/12" coloring={"black"} size={"h3"} variant={"tedx-sub"}>{sub}</Text>
                 <br />
